2dceeb75ea6d5e7853d11fab46f22774c1b30401
Thomas Poulsen <thomas.poulsen@broadlight.io>
2022-07-05 13:23:43 +0100

    Tidy database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-module.exports = ({env}) => ({
+module.exports = () => ({
   defaultConnection: 'default',
   connections: {
     default: {
@@ -12,7 +12,7 @@ module.exports = ({env}) => ({
         password: '{{DATABASE_PASSWORD}}',
         schema: 'public',
         ssl: false,
-      },      
+      },
       options: {},
     },
   },
